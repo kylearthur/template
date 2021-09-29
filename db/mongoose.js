0760013aad5dbf6715d7f4fdbaa15c03f5a99f84
kyle <gabkyle0814@gmail.com>
2021-09-29 12:58:32 +0800

    up

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:127.0.0.1:27017/test', {
+mongoose.connect('mongodb://localhost:127.0.0.1:27017/test1', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
