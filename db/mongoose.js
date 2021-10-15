b3093ab2a9baf63fdf8a6e184aceaecdebdd1597
kyle <gabkyle0814@gmail.com>
2021-10-15 14:01:33 +0800

    up

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:127.0.0.1:27017/test25/authSource=admin', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-})
+// mongoose.connect('mongodb://localhost:127.0.0.1:27017/test25', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// })
+mongoose.connect('mongodb://localhost/testdb').then(() => {
+console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
