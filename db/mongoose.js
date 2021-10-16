8061fae5e39e9b1846b0013499b1d6bdfba034c5
kyle <gabkyle0814@gmail.com>
2021-10-16 10:57:51 +0800

    up

--- db/mongoose.js
+++ db/mongoose.js
@@ -14,7 +14,7 @@ const mongoose = require('mongoose')
 //     console.log("Not Connected to Database ERROR! ", err);
 // });
 
-const url = 'mongodb://localhost/1'
+const url = 'mongodb://localhost/2'
 
 mongoose.connect(
     url,
